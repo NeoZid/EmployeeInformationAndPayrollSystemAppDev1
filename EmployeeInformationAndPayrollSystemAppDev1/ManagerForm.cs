58385a5ef9f4442dde5016f66419ea1f0988a161
NeoZid <zidereckn06@gmail.com>
2026-05-17 11:16:22 -0400

    datagrid translated

--- EmployeeInformationAndPayrollSystemAppDev1/ManagerForm.cs
+++ EmployeeInformationAndPayrollSystemAppDev1/ManagerForm.cs
@@ -48,6 +48,50 @@ namespace EmployeeInformationAndPayrollSystemAppDev1
 
             // hide sensitive data
             employeeDataView.Columns["Password"].Visible = false;
+
+            // rename columns based on current language
+            string culture = System.Threading.Thread.CurrentThread.CurrentUICulture.Name;
+
+            // translate info from the datagrid view
+            if (culture == "fr-CA")
+            {
+                employeeDataView.Columns["FirstName"].HeaderText = "Prénom";
+                employeeDataView.Columns["LastName"].HeaderText = "Nom de famille";
+                employeeDataView.Columns["EmployeeId"].HeaderText = "Identifiant";
+                employeeDataView.Columns["DateOfBirth"].HeaderText = "Date de naissance";
+                employeeDataView.Columns["Email"].HeaderText = "Courriel";
+                employeeDataView.Columns["Role"].HeaderText = "Rôle";
+                employeeDataView.Columns["Department"].HeaderText = "Département";
+                employeeDataView.Columns["HourlyRate"].HeaderText = "Taux horaire";
+                employeeDataView.Columns["HoursWorked"].HeaderText = "Heures travaillées";
+                employeeDataView.Columns["PTODays"].HeaderText = "Jours de congé";
+            }
+            else if (culture == "es")
+            {
+                employeeDataView.Columns["FirstName"].HeaderText = "Nombre";
+                employeeDataView.Columns["LastName"].HeaderText = "Apellido";
+                employeeDataView.Columns["EmployeeId"].HeaderText = "ID de empleado";
+                employeeDataView.Columns["DateOfBirth"].HeaderText = "Fecha de nacimiento";
+                employeeDataView.Columns["Email"].HeaderText = "Correo electrónico";
+                employeeDataView.Columns["Role"].HeaderText = "Rol";
+                employeeDataView.Columns["Department"].HeaderText = "Departamento";
+                employeeDataView.Columns["HourlyRate"].HeaderText = "Tarifa por hora";
+                employeeDataView.Columns["HoursWorked"].HeaderText = "Horas trabajadas";
+                employeeDataView.Columns["PTODays"].HeaderText = "Días de vacaciones";
+            }
+            else // English default
+            {
+                employeeDataView.Columns["FirstName"].HeaderText = "First Name";
+                employeeDataView.Columns["LastName"].HeaderText = "Last Name";
+                employeeDataView.Columns["EmployeeId"].HeaderText = "Employee ID";
+                employeeDataView.Columns["DateOfBirth"].HeaderText = "Date of Birth";
+                employeeDataView.Columns["Email"].HeaderText = "Email";
+                employeeDataView.Columns["Role"].HeaderText = "Role";
+                employeeDataView.Columns["Department"].HeaderText = "Department";
+                employeeDataView.Columns["HourlyRate"].HeaderText = "Hourly Rate";
+                employeeDataView.Columns["HoursWorked"].HeaderText = "Hours Worked";
+                employeeDataView.Columns["PTODays"].HeaderText = "PTO Days";
+            }
         }
 
         private void registerButton_Click(object sender, EventArgs e)
